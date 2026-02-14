2bdbb01bccbb03bc8d9c5ba8eb0ec7603790f9db
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2026-02-14 23:40:43 +0000

    add !within filter

--- src/routes/[slug]/VectorTileServer/resources/styles/root.json/makeStyleMetadata.ts
+++ src/routes/[slug]/VectorTileServer/resources/styles/root.json/makeStyleMetadata.ts
@@ -3,16 +3,44 @@ export default function makeStyleMetadata(style_json, tile_json) {
 	const layers = structuredClone(style_json.layers);
 	style_json.sources = {};
 	style_json.layers = [];
-	const source_key = Object.keys(sources).find(key => sources[key].type === 'vector');
+	const source_key = Object.keys(sources).find((key) => sources[key].type === 'vector');
 	style_json.sources.esri = { ...sources[source_key], tiles: tile_json.tiles, url: `../../` };
 
 	for (const layer of layers) {
 		if (!layer.source) style_json.layers.push(layer);
 		if (layer.source === source_key) {
-			layer.source = "esri";
+			layer.source = 'esri';
+			if (layer['source-layer'] === 'place')
+				layer.filter = [
+					'all',
+					[
+						'!',
+						[
+							'within',
+							{
+								type: 'Polygon',
+								coordinates: [
+									[
+										[34.143, 31.482],
+										[34.953, 29.419],
+										[35.427, 31.178],
+										[35.545, 31.761],
+										[35.573, 32.705],
+										[35.671, 33.249],
+										[35.545, 33.273],
+										[35.518, 33.092],
+										[34.966, 33.115],
+										[34.143, 31.482]
+									]
+								]
+							}
+						]
+					],
+					layer.filter
+				];
 			style_json.layers.push(layer);
 		}
 	}
 
-    return style_json;
+	return style_json;
 }
